904531e68459c68f8cf40fe6a9bd1bb04c648bdd
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-04-24 13:05:06 -0400

    chore: rename django project recipes -> recipes-core

--- recipes-core/asgi.py
+++ recipes-core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for recipes project.
+ASGI config for recipes-core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipes-core.settings')
 
 application = get_asgi_application()
--- recipes-core/urls.py
+++ recipes-core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for recipes project.
+URL configuration for recipes-core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
