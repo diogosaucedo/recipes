12125fca2127135266b15e2c347e7a5b0c3dc1a5
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-04-26 19:18:38 -0400

    feat: add view and search template

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
+from django.db.models import Q
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
 
-from .models import Recipe
+from recipes.models import Recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(
-        is_published=True
+        is_published=True,
     ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -24,13 +25,35 @@ def category(request, category_id):
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes[0].category.name} - Category | ',
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
+    })
+
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
     })
