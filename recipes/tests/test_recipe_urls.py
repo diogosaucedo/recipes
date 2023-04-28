7174158fabc7f9529e3e03fcf522899e40362608
Diogo Saucedo <Diogoferreiradfs@hotmail.com>
2023-04-28 13:25:27 -0400

    test: remove steps

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -4,8 +4,6 @@ from django.urls import reverse
 
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
         url = reverse('recipes:home')
         self.assertEqual(url, '/')
 
@@ -17,8 +15,8 @@ class RecipeURLsTest(TestCase):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
 
-        self.assertEqual(url, '/recipes/1/')
-
     def test_recipe_search_url_is_correct(self):
         url = reverse('recipes:search')
         self.assertEqual(url, '/recipes/search/')
+
+# RED - GREEN - REFACTOR
